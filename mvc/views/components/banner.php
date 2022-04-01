3194ae48aa31c0520d2b95e5ea02d134ac330128
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2022-04-01 17:17:26 +0700

    finish banner

--- mvc/views/components/banner.php
+++ mvc/views/components/banner.php
@@ -13,14 +13,14 @@
       <!-- The slideshow/carousel -->
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="./public/imgs/dungcugym.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+          <img src="./public/imgs/dungcugym.jpg" alt="Los Angeles" class="d-block" style="width:100vw; height: 100vh">
           <div class="carousel-caption banner">
             <h3>DỤNG CỤ TẬP GYM</h3>
             <p>Đa dạng dụng cụ thoải mái lựa chọn</p>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="./public/imgs/run.jpg" alt="Chicago" class="d-block" style="width:100%">
+          <img src="./public/imgs/run.jpg" alt="Chicago" class="d-block" style="width:100vw; height: 100vh">
           <div class="carousel-caption banner">
             <h3>THỰC PHẨM CHỨC NĂNG</h3>
             <p>Thực phẩm, đồ uống tăng hiệu quả cho việc luyện tập</p>
@@ -35,4 +35,36 @@
       <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
         <span class="carousel-control-next-icon"></span>
       </button>
-    </div>';
+    </div>
+    
+    <div class="awesome-thing">
+      <div class="thing">
+        <img class="thing-image" src="//cdn.shopify.com/s/files/1/0554/5784/1199/files/1_1e862949-1c29-4712-878e-b7fa9bf2a7cf.png?v=1639533201" alt="..."/>
+        <h4>
+          FRIENDLY SUPPORT
+        </h4>
+        <p>Hỗ trợ 24/7</p>
+      </div>
+      <div class="thing">
+        <img class="thing-image" src="//cdn.shopify.com/s/files/1/0554/5784/1199/files/3_4d31e578-9b6d-49ad-a774-fcb2912f74da.png?v=1639533202" alt="..."/>
+        <h4>
+          EASY RETURNS
+        </h4>
+        <p>Trả hàng sau 7 ngày nếu sản phẩm bị lỗi</p>
+      </div>
+      <div class="thing">
+        <img class="thing-image" src="//cdn.shopify.com/s/files/1/0554/5784/1199/files/4_dc75de55-5f72-4e3e-998a-b7208c2aaed8.png?v=1639533201" alt="..."/>
+        <h4>
+          QUALITY GUARANTEED
+        </h4>
+        <p>Chất lượng được đảm bảo</p>
+      </div>
+      <div class="thing">
+        <img class="thing-image" src="//cdn.shopify.com/s/files/1/0554/5784/1199/files/2_59cfc1e7-0a9b-44c7-ae33-1525f2950b01.png?v=1639533202" alt="..."/>
+        <h4>
+          FREE SHIPPING
+        </h4>
+        <p>Giao hàng miễn phí</p>
+      </div>
+    </div>
+    ';
